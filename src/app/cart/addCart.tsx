3e8fab2d58cb00db26ee53ff6c1d9b0f425bc6e8
shanahandnlf <shanahan.danualif@ui.ac.id>
2024-05-26 09:15:25 +0700

    init next js

---
+++ src/app/cart/addCart.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export default function AddCart() {
+    return (
+        <div>
+        <h1>Add Cart</h1>
+        </div>
+    );
+    }
---
+++ src/app/cart/page.tsx
@@ -0,0 +1,78 @@
+export const metadata = {
+    title: "Shopping Cart",
+  };
+  
+  type CartItem = {
+    id: number;
+    productId: string;
+    name: string;
+    quantity: number;
+    price: number;
+    cartItemId: number;
+  };
+  
+  type ShoppingCartResponse = {
+    totalPrice: number;
+    message: string;
+    cartItems: { [key: string]: CartItem };
+    userId: number;
+    voucherCode: string | null;
+  };
+  
+  async function getShoppingCart(userId: string) {
+    const res = await fetch(`http://localhost:8080/api/cart/data/${userId}`, {
+      cache: "no-store",
+    });
+    return res.json();
+  }
+  
+  export default async function cart() {
+    const userId = "2?voucherCode=ad179e86-8694-41c3-be00-5faafaa8ba93"; // Mock userId, replace with actual userId as needed
+    const cartData: ShoppingCartResponse = await getShoppingCart(userId);
+    const cartItems: CartItem[] = Object.values(cartData.cartItems);
+  
+    return (
+      <div className="py-10 px-10">
+        {/* <div className="py-2">
+          <AddProduct />
+        </div> */}
+        <table className="table w-full">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product Name</th>
+              <th>Price</th>
+              <th>Quantity</th>
+              <th>Total</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {cartItems.map((item, index) => (
+              <tr key={item.productId}>
+                <td>{index + 1}</td>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>{item.quantity}</td>
+                <td>{(item.price * item.quantity).toFixed(2)}</td>
+                {/* <td className="flex">
+                  <div className="mr-1">
+                    <UpdateProduct {...item} />
+                  </div>
+                  <DeleteProduct {...item} />
+                </td> */}
+              </tr>
+            ))}
+          </tbody>
+          <tfoot>
+            <tr>
+                <td colSpan={4} className="text-right">
+                    Total:
+                </td>
+                <td>{cartData.totalPrice.toFixed(2)}</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    );
+  }
